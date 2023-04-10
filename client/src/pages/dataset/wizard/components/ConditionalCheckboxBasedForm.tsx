b8da1acb96b0ea5e07a97891648019eac654851a
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-04-10 20:24:51 +0530

    state persistence

--- client/src/pages/dataset/wizard/components/ConditionalCheckboxBasedForm.tsx
+++ client/src/pages/dataset/wizard/components/ConditionalCheckboxBasedForm.tsx
@@ -19,7 +19,7 @@ const ConditionalCheckboxForm = (props: any) => {
     const [childFormValue, setChildFormValues] = useState<any>({});
 
     const filterPredicate = (field: any) => {
-        if (_.includes(_.get(existingState, 'checkboxSelection'), _.get(field, 'value'))) return true;
+        if (_.includes(_.get(existingState, 'formFieldSelection'), _.get(field, 'value'))) return true;
         if (_.get(field, ['selected']) === true) return true;
         return false;
     }
@@ -38,17 +38,15 @@ const ConditionalCheckboxForm = (props: any) => {
     const persistState = (state: Record<string, any>) => dispatch(addState({ id, ...state }));
     const formik = useFormik({ initialValues: getInitialValues(), onSubmit: values => { } });
     const formValues = formik.values;
+    const reset = () => persistState({});
+
+    const persist = () => {
+        const formFieldSelection = _.get(formValues, [name]);
+        persistState({ formFieldSelection, value: childFormValue });
+    }
 
     useEffect(() => {
-        const checkboxSelection = _.get(formValues, [name]);
-        const reset = () => persistState({});
-        if (!(checkboxSelection && Array.isArray(checkboxSelection))) reset();
-        const { type, ...rest } = childFormValue;
-        if (type && _.size(rest) > 1 && _.includes(checkboxSelection, type)) {
-            persistState({ checkboxSelection, ...childFormValue });
-        } else {
-            reset()
-        }
+        persist();
     }, [formValues, childFormValue]);
 
     const getFormType = (metadata: Record<string, any>) => {
@@ -85,7 +83,7 @@ const ConditionalCheckboxForm = (props: any) => {
             const { form, description, component, value: type } = metadata;
             return <>
                 {description && <Grid item xs={12}> <Alert color="info" icon={<InfoCircleOutlined />}> {description}</Alert></Grid>}
-                {form && <Grid item sm={12}> <MUIForm subscribe={setChildFormValues} initialValues={{ type, ...existingState }} onSubmit={(value: any) => onSubmission(value)} fields={form} size={{ sm: 6, xs: 6, lg: 6 }} /></Grid>}
+                {form && <Grid item sm={12}> <MUIForm subscribe={setChildFormValues} initialValues={{ type, ..._.get(existingState, 'value') }} onSubmit={(value: any) => onSubmission(value)} fields={form} size={{ sm: 6, xs: 6, lg: 6 }} /></Grid>}
                 {component && <Grid item sm={12}> {component}</Grid>}
             </>
         })
--- client/src/pages/dataset/wizard/components/ConditionalForm.tsx
+++ client/src/pages/dataset/wizard/components/ConditionalForm.tsx
@@ -4,39 +4,45 @@ import * as _ from 'lodash';
 import { Grid } from "@mui/material";
 import { Alert } from "@mui/material";
 import { InfoCircleOutlined } from "@ant-design/icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import config from 'data/initialConfig';
+import { addState } from "store/reducers/wizard";
 const { spacing } = config;
 
 const ConditionalForm = (props: any) => {
-    const { question, options, description, transform } = props;
-    const [response, subscribe] = useState<any>({});
+    const dispatch = useDispatch();
+    const { id, question, options, description, transform } = props;
+    const existingState: any = useSelector((state: any) => _.get(state, ['wizard', 'pages', id]) || ({}));
+    const { questionSelection, optionSelection } = existingState;
+    const [response, subscribe] = useState<any>(questionSelection || {});
+    const [childFormValue, setChildFormValues] = useState<any>(optionSelection || {});
     const selectedOption = _.get(response, _.get(question, 'name'));
     const onSubmission = (value: any) => { };
     const [config, setConfig] = useState<any>({});
     const [initialValue, setInitialValue] = useState<Record<string, any>>({});
     const redux = useSelector((state: any) => state);
 
-    const selectForm = (optionMeta: Record<string, any>) => {
+    const selectForm = () => {
+        const optionMeta = _.get(options, [selectedOption]);
         if (optionMeta) {
             const { form, description, size = { sm: 6, xs: 6, lg: 6 } } = optionMeta;
             setConfig({ form, description, size });
         }
     }
 
+    const persistState = (state: Record<string, any>) => dispatch(addState({ id, ...state }));
+
     useEffect(() => {
-        const optionMeta = _.get(options, [selectedOption]);
-        selectForm(optionMeta);
-    }, [selectedOption]);
+        selectForm();
+        persistState({ questionSelection: response, optionSelection: childFormValue });
+    }, [selectedOption, childFormValue]);
 
     return <>
         <Grid container rowSpacing={spacing}>
             {description && <Grid item xs={12}> <Alert color="info" icon={<InfoCircleOutlined />}> {description}</Alert></Grid>}
-            <Grid item xs={6}>
-                <MUIForm initialValues={{}} subscribe={subscribe} onSubmit={(value: any) => onSubmission(value)} fields={[question]} />
-            </Grid>
+            <Grid item xs={6}> <MUIForm initialValues={response} subscribe={subscribe} onSubmit={(value: any) => onSubmission(value)} fields={[question]} /></Grid>
             {_.get(config, 'description') && <Grid item xs={12}> <Alert color="info" icon={<InfoCircleOutlined />}> {_.get(config, 'description')}</Alert></Grid>}
-            {_.get(config, 'form') ? <Grid item sm={12}> <MUIForm initialValues={{}} onSubmit={(value: any) => onSubmission(value)} fields={_.get(config, 'form')} size={_.get(config, 'size')} /></Grid> : null}
+            {_.get(config, 'form') ? <Grid item sm={12}> <MUIForm subscribe={setChildFormValues} initialValues={childFormValue} onSubmit={(value: any) => onSubmission(value)} fields={_.get(config, 'form')} size={_.get(config, 'size')} /></Grid> : null}
         </Grid>
     </>
 }
