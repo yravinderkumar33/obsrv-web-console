f9e5da67d08458766b8ca9f0d71713232d66fab4
Praveen <praveen.veleneni@gmail.com>
2023-04-12 15:24:37 +0530

    #0 feat: add kafka connection test endpoint

--- src/shared/utils/fs.ts
+++ src/shared/utils/fs.ts
@@ -5,16 +5,16 @@ import fs from 'fs';
 import path from 'path';
 
 export const scrapModules = <Type extends { name: string }>(folderPath: string, basename: string) => {
-  const mapping = new Map<string, Omit<Type, 'name'>>();
-  fs.readdirSync(folderPath)
-    .filter((file) => file !== basename)
-    .forEach((file) => {
-      const {
-        default: { name, ...others },
-      } = require(path.join(folderPath, file)) as { default: Type };
+    const mapping = new Map<string, Omit<Type, 'name'>>();
+    fs.readdirSync(folderPath)
+        .filter((file) => file !== basename)
+        .forEach((file) => {
+            const {
+                default: { name, ...others },
+            } = require(path.join(folderPath, file)) as { default: Type };
 
-      mapping.set(name, others);
-    });
+            mapping.set(name, others);
+        });
 
-  return mapping;
+    return mapping;
 };
