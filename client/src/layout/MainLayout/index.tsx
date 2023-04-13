d0b5694822b5c6dcc927c241e8372c8da0a61f57
Praveen <praveen.veleneni@gmail.com>
2023-04-13 13:51:47 +0530

    #0 fix: feedback changes

--- client/src/layout/MainLayout/index.tsx
+++ client/src/layout/MainLayout/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
