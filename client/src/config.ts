777b51872396bc2215612866efef9848857a4bf3
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-04-17 14:17:26 +0530

    code cleanup

--- client/src/config.ts
+++ client/src/config.ts
@@ -7,18 +7,6 @@ export const twitterColor = '#1DA1F2';
 export const facebookColor = '#3b5998';
 export const linkedInColor = '#0e76a8';
 
-export const FIREBASE_API = {
-    apiKey: 'AIzaSyC74w_JCigORyepa_esLkPt-B3HgtI_X3o',
-    authDomain: 'mantis-4040b.firebaseapp.com',
-    projectId: 'mantis-4040b',
-    storageBucket: 'mantis-4040b.appspot.com',
-    messagingSenderId: '1073498457348',
-    appId: '1:1073498457348:web:268210e18c8f2cab30fc51',
-    measurementId: 'G-7SP8EXFS48'
-};
-
-// ==============================|| THEME CONFIG  ||============================== //
-
 const config: DefaultConfigProps = {
     defaultPath: '/',
     fontFamily: `'Public Sans', sans-serif`,
