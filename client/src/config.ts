9341ff0914a785898221aba8cf189ef12f782791
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-04-18 18:35:23 +0530

    remove unused code

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,6 @@
-// types
 import { DefaultConfigProps } from 'types/config';
 
 export const drawerWidth = 260;
-
-export const twitterColor = '#1DA1F2';
-export const facebookColor = '#3b5998';
-export const linkedInColor = '#0e76a8';
 
 const config: DefaultConfigProps = {
     defaultPath: '/',
--- client/src/utils/route-guard/AuthGuard.tsx
+++
@@ -1,7 +0,0 @@
-import { GuardProps } from 'types/auth';
-
-const AuthGuard = ({ children }: GuardProps) => {
-  return children;
-};
-
-export default AuthGuard;
