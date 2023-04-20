67a6bf478f72d4a60465bbaa09b9c1a906ae0b51
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-04-20 12:17:30 +0530

    session expiry interceptor added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,20 @@ import ThemeCustomization from 'themes';
 import Locales from 'components/Locales';
 import ScrollTop from 'components/ScrollTop';
 import Snackbar from 'components/@extended/Snackbar';
+import { useEffect } from 'react';
+import { addHttpRequestsInterceptor, errorInterceptor, responseInterceptor } from 'services/http';
+import { useNavigate } from 'react-router';
+import { useDispatch } from 'react-redux';
 
-const App = () => (
-  <ThemeCustomization>
+const App = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    addHttpRequestsInterceptor({ responseInterceptor, errorInterceptor: errorInterceptor({ navigate, dispatch }) })
+  }, [])
+
+  return <ThemeCustomization>
     <Locales>
       <ScrollTop>
         <>
@@ -15,6 +26,6 @@ const App = () => (
       </ScrollTop>
     </Locales>
   </ThemeCustomization>
-);
+}
 
 export default App;
--- client/src/services/http.ts
+++ client/src/services/http.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { error } from './toaster';
 
 const http = axios;
 
@@ -6,4 +7,27 @@ const request = (config: AxiosRequestConfig) => {
     return axios.request(config)
 }
 
-export { http, request };
+const responseInterceptor = (response: any) => response;
+
+const checkForSessionExpiry = (config: any) => {
+    const { navigate, status, dispatch } = config;
+    if (status === 401) {
+        dispatch(error({ message: "Unauthorized access !!" }));
+        navigate('/login');
+    }
+}
+
+const errorInterceptor = (config: any) => {
+    const { navigate, dispatch } = config;
+    return (error: any) => {
+        const { status } = error?.response;
+        checkForSessionExpiry({ navigate, status, dispatch });
+        Promise.reject(error)
+    }
+}
+
+const addHttpRequestsInterceptor = ({ responseInterceptor, errorInterceptor }: any) => {
+    http.interceptors.response.use(responseInterceptor, errorInterceptor)
+}
+
+export { http, request, responseInterceptor, errorInterceptor, addHttpRequestsInterceptor };
