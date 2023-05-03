384b7b26ca6f3949c1a89c499e7cf896cece4c66
Your Name <you@example.com>
2023-05-03 17:54:35 +0530

    auth folder changes

--- client/src/pages/auth/components/AuthLogin.tsx
+++ client/src/pages/auth/components/AuthLogin.tsx
@@ -9,115 +9,130 @@ import { useDispatch } from 'react-redux';
 import { error } from 'services/toaster';
 import { useSearchParams } from 'react-router-dom';
 import { interactIds } from 'data/telemetry/interactIds';
-import Social from './Socials';
+import LoginSocialButton from './LoginSocialButton';
 
 const AuthLogin = () => {
-  const dispatch = useDispatch();
-  const clientId = process.env.REACT_APP_OAUTH_CLIENT_ID || ""
-  const redirectURI = process.env.REACT_APP_WEB_CONSOLE_REDIRECT_URI || ""
-  const [showPassword, setShowPassword] = React.useState(false);
-  const [searchParams] = useSearchParams();
+    const dispatch = useDispatch();
+    const clientId = process.env.REACT_APP_OAUTH_CLIENT_ID || '';
+    const redirectURI = process.env.REACT_APP_WEB_CONSOLE_REDIRECT_URI || '';
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [searchParams] = useSearchParams();
 
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
 
-  const handleMouseDownPassword = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-  };
+    const handleMouseDownPassword = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+    };
 
-  useEffect(() => {
-    const err = searchParams.get("err");
-    if (err) {
-      dispatch(error({ message: err }));
-    }
-  }, [])
+    useEffect(() => {
+        const err = searchParams.get('err');
+        if (err) {
+            dispatch(error({ message: err }));
+        }
+    }, []);
 
-  return (
-    <>
-      <Formik
-        initialValues={{ username: '', password: '', submit: null }}
-        validationSchema={Yup.object().shape({ username: Yup.string().max(255).required('User name is required'), password: Yup.string().max(255).required('Password is required') })}
-        onSubmit={async () => { }}
-      >
-        {({ errors, handleBlur, handleChange, isSubmitting, touched, values }) => (
-          <form noValidate action='/api/oauth/authorize' method='post'>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="email-login">User Name</InputLabel>
-                  <OutlinedInput
-                    id="email-login"
-                    type="text"
-                    value={values.username}
-                    name="username"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    placeholder="Enter user name"
-                    fullWidth
-                    error={Boolean(touched.username && errors.username)}
-                  />
-                </Stack>
-              </Grid>
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <InputLabel htmlFor="password-login">Password</InputLabel>
-                  <input type="hidden" value={clientId} name="client_id" />
-                  <input type="hidden" value={redirectURI} name="redirect_uri" />
-                  <input type="hidden" value="code" name="response_type" />
-                  <input type="hidden" value="authorization_code" name="grant_type" />
-                  <OutlinedInput
-                    fullWidth
-                    color={'primary'}
-                    error={Boolean(touched.password && errors.password)}
-                    id="-password-login"
-                    type={showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    name="password"
-                    onBlur={(event: React.FocusEvent<any, Element>) => {
-                      handleBlur(event);
-                    }}
-                    onChange={handleChange}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          id="input-password"
-                          data-edataId={showPassword ? interactIds.button.input.password.visible : interactIds.button.input.password.invisible}
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                          color="secondary"
-                        >
-                          {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                    placeholder="Enter password"
-                  />
-                </Stack>
-              </Grid>
-              <Grid item xs={12}>
-                <AnimateButton>
-                  <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
-                    Login
-                  </Button>
-                </AnimateButton>
-              </Grid>
-              <Grid item xs={12}>
+    return (
+        <>
+            <Formik
+                initialValues={{ username: '', password: '', submit: null }}
+                validationSchema={Yup.object().shape({
+                    username: Yup.string().max(255).required('User name is required'),
+                    password: Yup.string().max(255).required('Password is required')
+                })}
+                onSubmit={async () => {}}
+            >
+                {({ errors, handleBlur, handleChange, isSubmitting, touched, values }) => (
+                    <form noValidate action="/api/oauth/authorize" method="post">
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="email-login">User Name</InputLabel>
+                                    <OutlinedInput
+                                        id="email-login"
+                                        type="text"
+                                        value={values.username}
+                                        name="username"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        placeholder="Enter user name"
+                                        fullWidth
+                                        error={Boolean(touched.username && errors.username)}
+                                    />
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="password-login">Password</InputLabel>
+                                    <input type="hidden" value={clientId} name="client_id" />
+                                    <input type="hidden" value={redirectURI} name="redirect_uri" />
+                                    <input type="hidden" value="code" name="response_type" />
+                                    <input type="hidden" value="authorization_code" name="grant_type" />
+                                    <OutlinedInput
+                                        fullWidth
+                                        color={'primary'}
+                                        error={Boolean(touched.password && errors.password)}
+                                        id="-password-login"
+                                        type={showPassword ? 'text' : 'password'}
+                                        value={values.password}
+                                        name="password"
+                                        onBlur={(event: React.FocusEvent<any, Element>) => {
+                                            handleBlur(event);
+                                        }}
+                                        onChange={handleChange}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    id="input-password"
+                                                    data-edataId={
+                                                        showPassword
+                                                            ? interactIds.button.input.password.visible
+                                                            : interactIds.button.input.password.invisible
+                                                    }
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                    color="secondary"
+                                                >
+                                                    {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                        placeholder="Enter password"
+                                    />
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <AnimateButton>
+                                    <Button
+                                        disableElevation
+                                        disabled={isSubmitting}
+                                        fullWidth
+                                        size="large"
+                                        type="submit"
+                                        variant="contained"
+                                        color="primary"
+                                    >
+                                        Login
+                                    </Button>
+                                </AnimateButton>
+                            </Grid>
+                            <Grid item xs={12}>
                                 <Divider>
                                     <Typography variant="caption"> Login with</Typography>
                                 </Divider>
                             </Grid>
                             <Grid item xs={12}>
-                                <Social />
+                                <LoginSocialButton />
                             </Grid>
-            </Grid>
-          </form>
-        )}
-      </Formik>
-    </>
-  );
+                        </Grid>
+                    </form>
+                )}
+            </Formik>
+        </>
+    );
 };
 
 export default AuthLogin;
