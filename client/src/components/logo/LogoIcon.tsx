2408c2eb468250a194f5c7d5b32e26aa2d2eead6
Praveen <praveen.veleneni@gmail.com>
2023-05-05 13:42:32 +0530

    #4 fix: logo update

--- client/src/components/logo/LogoIcon.tsx
+++ client/src/components/logo/LogoIcon.tsx
@@ -1,12 +1,12 @@
 import { useTheme } from '@mui/material/styles';
-import logoIcon from 'assets/images/half_logo.png';
+import logoIcon from 'assets/images/obsrv-logo.svg';
 
 const LogoIcon = () => {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  return (
-    <img src={theme.palette.mode === 'dark' ? logoIcon : logoIcon} width="50" />
-  );
+    return (
+        <img src={theme.palette.mode === 'dark' ? logoIcon : logoIcon} width="50" />
+    );
 };
 
 export default LogoIcon;
