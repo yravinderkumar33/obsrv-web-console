950c8f222c46ed8971b3c4b9e6f33f3300f7fdd4
Praveen <praveen.veleneni@gmail.com>
2023-05-08 13:14:39 +0530

    #4 fix: reduce margin for error display

--- client/src/components/third-party/dropzone/RejectionFiles.tsx
+++ client/src/components/third-party/dropzone/RejectionFiles.tsx
@@ -7,38 +7,38 @@ import { FileRejection } from 'react-dropzone';
 import { getDropzoneData } from './FilesPreview';
 
 type Props = {
-  fileRejections: FileRejection[];
+    fileRejections: FileRejection[];
 };
 
 export default function RejectionFiles({ fileRejections }: Props) {
-  return (
-    <Paper
-      variant="outlined"
-      sx={{
-        py: 1,
-        px: 2,
-        mt: 3,
-        borderColor: 'error.light',
-        bgcolor: (theme) => alpha(theme.palette.error.main, 0.08)
-      }}
-    >
-      {fileRejections.map(({ file, errors }) => {
-        const { path, size } = getDropzoneData(file);
+    return (
+        <Paper
+            variant="outlined"
+            sx={{
+                py: 1,
+                px: 2,
+                mt: 1,
+                borderColor: 'error.light',
+                bgcolor: (theme) => alpha(theme.palette.error.main, 0.08)
+            }}
+        >
+            {fileRejections.map(({ file, errors }) => {
+                const { path, size } = getDropzoneData(file);
 
-        return (
-          <Box key={path} sx={{ my: 1 }}>
-            <Typography variant="subtitle2" noWrap>
-              {path} - {size ? size : ''}
-            </Typography>
+                return (
+                    <Box key={path} sx={{ my: 1 }}>
+                        <Typography variant="subtitle2" noWrap>
+                            {path} - {size ? size : ''}
+                        </Typography>
 
-            {errors.map((error) => (
-              <Box key={error.code} component="li" sx={{ typography: 'caption' }}>
-                {error.message}
-              </Box>
-            ))}
-          </Box>
-        );
-      })}
-    </Paper>
-  );
+                        {errors.map((error) => (
+                            <Box key={error.code} component="li" sx={{ typography: 'caption' }}>
+                                {error.message}
+                            </Box>
+                        ))}
+                    </Box>
+                );
+            })}
+        </Paper>
+    );
 }
