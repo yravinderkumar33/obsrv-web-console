1e29f7da9d15f06d4e3c4f9f9a1e59625fa84103
Praveen <praveen.veleneni@gmail.com>
2023-05-08 17:53:30 +0530

    #4 fix: update batch validation

--- client/src/data/forms/input.ts
+++ client/src/data/forms/input.ts
@@ -43,7 +43,7 @@ export const batchForm = [
             label: 'Enable Deduplication',
             value: 'yes'
         }],
-        validationSchema: yup.array().required('This field is required'),
+        validationSchema: yup.array().optional(),
     },
     {
         name: "dedupePeriod",
@@ -55,7 +55,11 @@ export const batchForm = [
             key: "dedupeRequired",
             value: 'yes'
         },
-        validationSchema: yup.number().required('This field is required'),
+        validationSchema: yup.number().when(
+            'dedupeRequired', {
+            is: (option: any) => option.includes('yes'),
+            then: yup.number().required('This field is required')
+        }),
     }
 ]
 
