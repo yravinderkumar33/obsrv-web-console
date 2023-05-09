4111921556a400cd0d2df0e65bfb5aeeb586efa1
yravinderkumar33 <yravinderkumar33@gmail.com>
2023-05-09 12:51:36 +0530

    Issue #0000 feat: deep storage metrics added

--- client/src/data/metrics.tsx
+++ client/src/data/metrics.tsx
@@ -56,7 +56,7 @@ export const metricsMetadata = [
                         </AnalyticsDataCard>
                     },
                     {
-                        id:"memoryUsage",
+                        id: "memoryUsage",
                         description: "Current Memory Usage Percent",
                         chart: <AnalyticsDataCard title="Memory Usage">
                             <GaugeChart query={_.get(chartMeta, 'memory_usage_radial.query')} />
@@ -65,7 +65,7 @@ export const metricsMetadata = [
                         </AnalyticsDataCard>
                     },
                     {
-                        id:"diskUsage",
+                        id: "diskUsage",
                         description: "Current Disk Usage Percent",
                         chart: <AnalyticsDataCard title="Disk Usage">
                             <GaugeChart arcsLength={[60, 20, 20]} query={_.get(chartMeta, 'disk_usage_radial.query')} />
@@ -392,12 +392,16 @@ export const metricsMetadata = [
                     xs: 12,
                     sm: 6,
                     md: 4,
-                    lg: 3
+                    lg: 4
                 },
                 metadata: [
                     {
                         description: "This chart shows the current success backup count",
                         chart: <ReportCard primary="0" secondary="Success Backups Count" iconPrimary={BarChartOutlined} query={_.get(chartMeta, 'backup_count.query')} />
+                    },
+                    {
+                        description: "This chart shows the total size of deep storage.",
+                        chart: <ReportCard primary="0" secondary="Deep Storage" iconPrimary={BarChartOutlined} query={_.get(chartMeta, 'deep_storage_total.query')} />
                     }
                 ]
             },
